bbc2975ac66e4b1b09229c86facb93bf153fcdc5
PMaloney15 <122317498+PMaloney15@users.noreply.github.com>
2023-03-23 16:08:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,27 +11,31 @@ import game
 
 class Main:
     def play(self):
-        print('Combat!')
-        time.sleep(1)
-        print(f'Fighting a(n) {game.Monster.type(self, random.randint(1, 4))}!\n')  # initializes a random monster
-        time.sleep(1)
-        fight = game.Game()
-        initiative = random.randint(1, 2)  # randomly decide who acts first, player on 1, monster on 2
-        if initiative == 1:
-            game.Game.player_act(self)
-            while game.Player.hp > 0 and game.Monster.mon_hp > 0:  # while loops use counter to continue the fight
-                if game.Game.counter % 2 != 0:
-                    game.Game.monster_act(self)
-                else:
-                    game.Game.player_act(self)
-        else:
-            game.Game.monster_act(self)
-            while game.Player.hp > 0 and game.Monster.mon_hp > 0:
-                time.sleep(1)
-                if game.Game.counter % 2 != 0:
-                    game.Game.player_act(self)
-                else:
-                    game.Game.monster_act(self)
+        while game.Game.cont:
+            game.Game.counter = 0  # ensures counter is reset with each loop
+            game.Game.cont_check = 'Invalid'  # ensures continue check runs
+            print('Combat!')
+            time.sleep(1)
+            game.get_monster()  # initializes a random monster
+            print(f'Fighting a(n) {game.Monster.monster}!\n')
+            time.sleep(1)
+            fight = game.Game()
+            initiative = random.randint(1, 2)  # randomly decide who acts first, player on 1, monster on 2
+            if initiative == 1:
+                game.Game.player_act(self)
+                while game.Player.hp > 0 and game.Monster.mon_hp > 0:  # while loops use counter to continue the fight
+                    if game.Game.counter % 2 != 0:
+                        game.Game.monster_act(self)
+                    else:
+                        game.Game.player_act(self)
+            else:
+                game.Game.monster_act(self)
+                while game.Player.hp > 0 and game.Monster.mon_hp > 0:
+                    time.sleep(1)
+                    if game.Game.counter % 2 != 0:
+                        game.Game.player_act(self)
+                    else:
+                        game.Game.monster_act(self)
 
 
 game1 = Main()
